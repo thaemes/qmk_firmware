bd5e086ca7616397e5ce02f6045b7c2c3f5150e1
Jan Kolkmeier <jankolkmeier@gmail.com>
2019-10-20 20:40:28 +0200

    BMEK v2

---
+++ keyboards/bemeier/bmek/README.md
@@ -0,0 +1,15 @@
+# BMEK - Bemeier Ergonomic Keyboard
+
+![BMEK](https://i.imgur.com/odvl8UO.png)
+
+BMEK is a HHKB-like version of TGR Alice.
+PCB and case design files are publicly available.
+
+Maintainer: [Bemeier](https://github.com/Bemeier)  
+Hardware: [BMEK Hardware](https://github.com/Bemeier/bmek)
+
+Make example for this keyboard (after setting up your build environment):
+
+    make bemeier/bmek:default
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
---
+++ keyboards/bemeier/bmek/bmek.h
@@ -0,0 +1,34 @@
+/* Copyright 2019 vvv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "quantum.h"
+
+#define LAYOUT(\
+    KC_40, KC_41, KC_42, KC_43, KC_44, KC_45, KC_46, KC_47,     KC_48, KC_49, KC_4A, KC_4B, KC_4C, KC_4D, KC_4E, KC_3E,\
+   KC_30,  KC_31,  KC_32, KC_33, KC_34, KC_35, KC_36,         KC_37, KC_38, KC_39, KC_3A, KC_3B, KC_3C, KC_3D,    KC_2E,\
+  KC_20,   KC_21,   KC_22, KC_23, KC_24, KC_25, KC_26,         KC_27, KC_28, KC_29, KC_2A, KC_2B,     KC_2C,       KC_2D,\
+ KC_10,    KC_11,     KC_12,  KC_13, KC_14, KC_15, KC_16,    KC_17, KC_18, KC_19, KC_1A,  KC_1B, KC_1C,  KC_1D,     KC_1E,\
+                      KC_02,  KC_03,        KC_05,                  KC_08,        KC_0A,  KC_0B                            \
+   ) { \
+   { KC_NO, KC_NO, KC_02, KC_03, KC_NO, KC_05, KC_NO, KC_NO, KC_08, KC_NO, KC_0A, KC_0B, KC_NO, KC_NO, KC_NO },\
+   { KC_10, KC_11, KC_12, KC_13, KC_14, KC_15, KC_16, KC_17, KC_18, KC_19, KC_1A, KC_1B, KC_1C, KC_1D, KC_1E },\
+   { KC_20, KC_21, KC_22, KC_23, KC_24, KC_25, KC_26, KC_27, KC_28, KC_29, KC_2A, KC_2B, KC_2C, KC_2D, KC_2E },\
+   { KC_30, KC_31, KC_32, KC_33, KC_34, KC_35, KC_36, KC_37, KC_38, KC_39, KC_3A, KC_3B, KC_3C, KC_3D, KC_3E },\
+   { KC_40, KC_41, KC_42, KC_43, KC_44, KC_45, KC_46, KC_47, KC_48, KC_49, KC_4A, KC_4B, KC_4C, KC_4D, KC_4E } \
+}
+
+
